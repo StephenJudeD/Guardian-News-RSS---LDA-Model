2e6abacb835b12a62863857d6a175d8c274ac9f9
StephenJD <105487389+StephenJudeD@users.noreply.github.com>
2025-03-10 15:03:47 +0000

    Update guardian_fetcher.py

--- guardian_fetcher.py
+++ guardian_fetcher.py
@@ -53,7 +53,7 @@ class GuardianFetcher:
                     total_pages = data['pages']
     
                 for article in data['results']:
-                    if 'fields' in article and article['sectionName'] in ["World news", "US news", "Football", "Sport", "UK news", "Global development"]:
+                    if 'fields' in article and article['sectionName'] in ["World news", "US news", "Politics", "Opinion", "UK news", "Global development"]:
                         all_articles.append({
                             'title': article['webTitle'],
                             'content': article['fields'].get('bodyText', ''),
